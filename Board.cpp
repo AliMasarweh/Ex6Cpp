e598014f052b17601af98b682c357ecc93315efd
AliMasarweh <33619630+AliMasarweh@users.noreply.github.com>
2018-05-09 22:23:29 +0300

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -20,29 +20,6 @@ Board::Board(const Board& newBoard) {
 	}
 }
 
-Board::~Board() {
-	int i;
-	for (i = 0; i< this->size; i++)
-		delete[] board[i]; //This part was allocated with new[], so we use delete[]
-	delete[] board;
-}
-
-int Board::getSize() const {
-	return this->size;
-}
-
-void Board::setSize(int newSize) {
-	Board *tmp = new Board(newSize);
-	for (int i = 0; i < size && i < newSize; i++)
-		for (int j = 0; j < size && j < newSize; j++)
-			tmp[i,j] = this[i,j];
-	for (int i = size; i < newSize; i++)
-		for (int j = size; j < newSize; j++)
-			tmp[i, j] = '.';
-	*this = *tmp;
-	size = newSize;
-}
-
 Board& Board::operator=(const Board& newBoard) {
 	this->~Board();
 	size = newBoard.size;
@@ -51,7 +28,7 @@ Board& Board::operator=(const Board& newBoard) {
 	for (i = 0; i < size; i++) {
 		board[i] = new Status[size];
 		for (j = 0; j < size; j++)
-			this[i, j] = (&newBoard)[i, j];
+			board[i][j] = newBoard.board[i][j];
 	}
 	return *this;
 }
@@ -61,7 +38,7 @@ Board& Board::operator= (const char newStatus) {
 	int i, j;
 	for (i = 0; i < this->size; i++)
 		for (j = 0; j < this->size; j++)
-			this[i,j] = status;
+			this->board[i][j] = status;
 	return *this;
 }
 
@@ -75,8 +52,16 @@ ostream& operator<< (ostream& os, const Board& inserteBoard) {
 	int i, j;
 	for (i = 0; i < inserteBoard.size; i++) {
 		for (j = 0; j < inserteBoard.size; j++)
-			os << inserteBoard.board[i,j];
+			os << inserteBoard.board[i][j];
 		os << endl;
 	}
 	return os;
 }
+
+Board::~Board() {
+	int i;
+	for (i = 0; i< this->size; i++)
+		delete[] board[i]; //This part was allocated with new[], so we use delete[]
+	delete[] board;
+}
+
