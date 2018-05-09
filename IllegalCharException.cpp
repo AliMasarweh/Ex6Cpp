c24a2d7175e5ca5e3236e302515e6905a5bb94cb
AliMasarweh <33619630+AliMasarweh@users.noreply.github.com>
2018-05-09 22:05:32 +0300

    Update IllegalCharException.cpp

--- IllegalCharException.cpp
+++ IllegalCharException.cpp
@@ -1,12 +1,12 @@
 
 class IllegalCharException {
 private:
-	const char c;
+	const char _char;
 
 public:
-	IllegalCharException(const char c) : c(c) { }
+	IllegalCharException(const char illigalChar) : _char(illigalChar) { }
 
 	const char theChar() const {
-		return this->c;
+		return this->_char;
 	}
 };
