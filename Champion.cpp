be8d8dfd0ea4c11c0304d7157dbfad5c928f4c61
AliMasarweh <33619630+AliMasarweh@users.noreply.github.com>
2018-05-18 12:16:17 +0300

    Update Champion.cpp

--- Champion.cpp
+++ Champion.cpp
@@ -2,47 +2,48 @@
 
 
 const Coordination Champion::play(const Board& board) {
-	if (board[{0, board.size()-1}] == '.')
-		return Coordination{ 0, board.size() - 1 };
-	if (board[{ board.size() - 1, 0 }] == '.')
-		return Coordination{ board.size() - 1,0 };
-	for (int i = 1; i < board.size() - 1; i++) {
-		if (board[{ board.size() - i - 1, i }] == '.')
-			return Coordination{ board.size() - i - 1,i };
+	int boardSize = board.BoardSize() - 1;
+	if (board[{0, boardSize}] == '.')
+		return Coordination{ 0, board.BoardSize() - 1 };
+	if (board[{ boardSize, 0 }] == '.')
+		return Coordination{ boardSize,0 };
+	for (int i = 1; i < boardSize; i++) {
+		if (board[{ boardSize - i, i }] == '.')
+			return Coordination{ board.BoardSize() - i - 1,i };
 	}
-	return { board.size() - 1, board.size() - 1 };
+	return { boardSize, boardSize };
 }
 
 
 
-/*Coordination c{ board.size()-1, board.size() - 1 };
+/*Coordination c{ board.BoardSize()-1, board.BoardSize() - 1 };
 if (board[{c.getXCoordination(), c.getYCoordination()}] == '.')
 return c;
 if (board[{c.getXCoordination(), c.getYCoordination()}] != '.' && board[{ 1, 0 }] == '.' && board[{ 0, 1 }] == '.') {
-c.setYCoordination(board.size() - 2);
+c.setYCoordination(board.BoardSize() - 2);
 return c;
 }
 if (board[{ 1,0 }] != '.' && board[{ 0,1 }] == '.') {
-Coordination temp{ board.size() - 1,0 };
+Coordination temp{ board.BoardSize() - 1,0 };
 if (board[{temp.getXCoordination(),temp.getYCoordination()}] != '.')
 return temp;//Write on the (n,0) coordination, preventing the XYPlayer from winning and ensuring champion victory
 }
 if (board[{ 0,1 }] != '.' && board[{ 1,0 }] == '.') {
-Coordination temp{ 0,board.size() - 1 };
+Coordination temp{ 0,board.BoardSize() - 1 };
 if (board[{temp.getXCoordination(),temp.getYCoordination()}] != '.')
 return temp;//Write on the (0,n) coordination, preventing the YXPlayer from winning and ensuring champion victory
 }
-if (board[{ 0,board.size() - 1 }] == '.') {
-Coordination temp{ board.size() - 1,0 };
-for (int i = 1; i < board.size() - 1; i++) {
+if (board[{ 0,board.BoardSize() - 1 }] == '.') {
+Coordination temp{ board.BoardSize() - 1,0 };
+for (int i = 1; i < board.BoardSize() - 1; i++) {
 temp.setYCoordination(i);
 if (board[{temp.getXCoordination(),temp.getYCoordination()}] != '.')
 return temp;//Write on the (n,i) coordination 1 < i < n-1, dominating the XYPlayer
 }
 }
-if (board[{ board.size() - 1,0 }] == '.') {
-Coordination temp{ 0,board.size() - 1 };
-for (int i = 1; i < board.size() - 1; i++) {
+if (board[{ board.BoardSize() - 1,0 }] == '.') {
+Coordination temp{ 0,board.BoardSize() - 1 };
+for (int i = 1; i < board.BoardSize() - 1; i++) {
 temp.setXCoordination(i);
 if (board[{temp.getXCoordination(),temp.getYCoordination()}] != '.')
 return temp;//Write on the (i,n) coordination 1 < i < n-1, dominating the YXPlayer
